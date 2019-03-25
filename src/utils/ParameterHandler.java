51078852b624248b3a7b41a57dbf74fc23ada95f
Boshrahi7 <bhashemi@ce.sharif.edu>
2019-03-25 22:22:27 +0430

    change region user number method

--- src/utils/ParameterHandler.java
+++ src/utils/ParameterHandler.java
@@ -28,14 +28,13 @@ import java.util.Random;
 public class ParameterHandler {
 
     public int numOfVRCPerApp;
-    public int numOfUsers;
     public int numOfApps;
     private String numberOfUsersPerRegion;
     public long totalRequests;
     private final double alpha = 1; //for all
-    private final double cloudDelay = 400; //ms
+    private final double cloudDelay = 1000; //ms
     final double serviceDelayPerRegion = 0.01; //ms
-    private final double serviceRate = 109000; //3010; // 57
+    private final double serviceRate = 10000; //3010; // 57
     private final String capacityOfMec = "1000"; //should be 1000
     //------------------- demand of every request of app
     private final int demandOfRequest = 2;
@@ -49,13 +48,11 @@ public class ParameterHandler {
 
     public ParameterHandler(int numOfVRCPerApp, int numOfUsers, int numOfApps, GraphModel graphModel) {
         this.numOfApps = numOfApps;
-        this.numOfUsers = numOfUsers;
         this.numOfVRCPerApp = numOfVRCPerApp;
         this.graphModel = graphModel;
         this.numberOfUsersPerRegion = calculateNumberOfUsersPerRegion(numOfUsers, graphModel);
         this.landaPerApp = calculateLandaPerAppForUsers(numOfApps);
         this.totalRequests = calculateTotalRequests(numberOfUsersPerRegion, landaPerApp);
-        //this.totalRequests = 11378;
         initializeCapacityOfMEC();
 
     }
@@ -91,7 +88,7 @@ public class ParameterHandler {
     private int getDemandOfApp(int whichApp) {
         if (whichApp == 0) return demandOfRequest;
         else if (whichApp == 1) return 2 * demandOfRequest;
-        else return 3*demandOfRequest;
+        else return 3 * demandOfRequest;
     }
 
     public double getCapacityOfMEC(int whichRegion, int whichApp) {
@@ -127,7 +124,7 @@ public class ParameterHandler {
         double average = 0;
         String[] users_region = numberOfUsersPerRegion.split(",");
         for (int nodeIndex = 0; nodeIndex < graphModel.nodeNum; nodeIndex++) {
-            if (isPlacementHaveQ(whichRegion,vm_placement.split(","))) {
+            if (isPlacementHaveQ(whichRegion, vm_placement.split(","))) {
                 indicator = 1;
             }
             for (int sigmaIndex = 0; sigmaIndex < sigmaList.size(); sigmaIndex++) {
@@ -149,11 +146,10 @@ public class ParameterHandler {
             A_region_V = A_region_V + calculateAvrgRequestArrivalRate(appIndex, whichRegion, list);
             //System.out.println("A_region_V : " + A_region_V);
         }
-        if (serviceRate <= A_region_V){
+        if (serviceRate <= A_region_V) {
             System.out.println("A_region_V : " + A_region_V);
             throw new IllegalArgumentException();
-        }
-        else{
+        } else {
             t = 1 / (serviceRate - A_region_V);
             //System.out.println("service time : " + t);
             return t;
@@ -195,10 +191,10 @@ public class ParameterHandler {
         String[] users_region = numberOfUsersPerRegion.split(",");
         String[] landa_app = landaPerApp.split(",");
         long total = 0;
-        for (int index = 0; index < users_region.length; index++) {
+        for (int index = 0; index < graphModel.nodeNum; index++) {
             for (int appIndex = 0; appIndex < landa_app.length; appIndex++) {
                 total = (long) (total + calculateRequestOfAppInRegionV(appIndex
-                                        , index));
+                        , index));
             }
         }
         return total;
@@ -255,21 +251,68 @@ public class ParameterHandler {
     private String calculateNumberOfUsersPerRegion(int numOfUsers, GraphModel graphModel) {
 //        int num_node = graphModel.nodeNum;
 //        String userNumber = "";
+//        int max = 16;
+//        int min = 10;
 //        for (int i = 0; i < num_node; i++) {
-//            int randomNum = (1+ new Random().nextInt(10));
-//            int numberOfUserPerRegion = (numOfUsers / num_node)* randomNum;
+//
+//            int randomNum = new Random().nextInt(max - min + 1) + min;
+//            int numberOfUserPerRegion = randomNum;
 //            userNumber = userNumber + numberOfUserPerRegion + ",";
 //        }
 //        return userNumber;
+        if (graphModel.graphName.equals(Graph.SPIRALIGHT)) {
+            String str = "" + 53 * numOfUsers + "," + 61 * numOfUsers + "," + 50 * numOfUsers + "," + 55 * numOfUsers + "," + 68 * numOfUsers + "," + 61 * numOfUsers +
+                    "," + 53 * numOfUsers + "," + 68 * numOfUsers + "," + 58 * numOfUsers + "," + 57 * numOfUsers + "," + 54 * numOfUsers + "," + 52 * numOfUsers +
+                    "," + 68 * numOfUsers + "," + 69 * numOfUsers + "," + 69 * numOfUsers;
+            return str;
+        } else if (graphModel.graphName.equals(Graph.SAGO)) {
+            String str = "" + 51 * numOfUsers + "," + 48 * numOfUsers + "," + 51 * numOfUsers + "," + 48 * numOfUsers + "," + 51 * numOfUsers + "," + 50 * numOfUsers +
+                    "," + 51 * numOfUsers + "," + 46 * numOfUsers + "," + 56 * numOfUsers + "," + 44 * numOfUsers + "," + 53 * numOfUsers + "," + 60 * numOfUsers +
+                    "," + 59 * numOfUsers + "," + 40 * numOfUsers + "," + 43 * numOfUsers + "," + 46 * numOfUsers + "," + 58 * numOfUsers + "," + 49 * numOfUsers;
+            return str;
 
-        //Sago
-        //return "250,1000,700,150,250,433,70,150,250,100,7,150,350,244,7000,10,50,144";
-        //Noel
-        return "25,100,70,15,25,43,7,15,25,10,2,15,35,24,700,1,5,14,7";
-        //Test
-        //return "250,1000,700,150";
+        } else if (graphModel.graphName.equals(Graph.SHENTEL)) {
+            String str = "" + 35 * numOfUsers + "," + 39 * numOfUsers + "," + 37 * numOfUsers + "," + 31 * numOfUsers + "," + 33 * numOfUsers + "," + 37 * numOfUsers +
+                    "," + 36 * numOfUsers + "," + 40 * numOfUsers + "," + 32 * numOfUsers + "," + 37 * numOfUsers + "," + 34 * numOfUsers + "," + 40 * numOfUsers +
+                    "," + 36 * numOfUsers + "," + 37 * numOfUsers + "," + 34 * numOfUsers + "," + 40 * numOfUsers + "," + 36 * numOfUsers + "," + 36 * numOfUsers + "," + 36 * numOfUsers+ "," + 36 * numOfUsers
+                    + "," + 37 * numOfUsers+ "," + 34 * numOfUsers+ "," + 40 * numOfUsers+ "," + 32 * numOfUsers+ "," + 39 * numOfUsers+ "," + 37 * numOfUsers+ "," + 35 * numOfUsers+ "," + 34 * numOfUsers+ ","
+                    + 34 * numOfUsers;
+            return str;
+
+        } else if (graphModel.graphName.equals(Graph.MISSOURI)) {
+                    String str = "" + 10 * numOfUsers + "," + 16 * numOfUsers + "," + 15 * numOfUsers + "," + 10 * numOfUsers + "," + 12 * numOfUsers + "," + 10 * numOfUsers +
+                    "," + 11 * numOfUsers + "," + 16 * numOfUsers + "," + 15 * numOfUsers + "," + 14 * numOfUsers + "," + 15 * numOfUsers + "," + 14 * numOfUsers +
+                    "," + 13 * numOfUsers + "," + 15 * numOfUsers + "," + 14 * numOfUsers + "," + 16 * numOfUsers + "," + 15 * numOfUsers + "," + 13 * numOfUsers + "," + 12 * numOfUsers+ "," + 10 * numOfUsers
+                    + "," + 12 * numOfUsers+ "," + 13 * numOfUsers+ "," + 12 * numOfUsers+ "," + 10 * numOfUsers+ "," + 13 * numOfUsers+ "," + 16 * numOfUsers+ "," + 14 * numOfUsers+ "," + 15 * numOfUsers
+                    + "," + 11 * numOfUsers + "," + 13 * numOfUsers+ "," + 10 * numOfUsers+ "," + 15 * numOfUsers+ "," + 14 * numOfUsers+ "," + 16 * numOfUsers+ "," + 12 * numOfUsers+ "," + 10 * numOfUsers
+                            + "," + 15 * numOfUsers
+                    + "," + 12 * numOfUsers+ "," + 10 * numOfUsers+ "," + 10 * numOfUsers+ "," + 12 * numOfUsers+ "," + 11 * numOfUsers+ "," + 10 * numOfUsers+ "," + 12 * numOfUsers+ "," + 10 * numOfUsers
+                    + "," + 11 * numOfUsers+ "," + 14 * numOfUsers+ "," + 16 * numOfUsers+ "," + 10 * numOfUsers+ "," + 13 * numOfUsers+ "," + 15 * numOfUsers+ "," + 11 * numOfUsers+ "," + 16 * numOfUsers
+                    + "," + 10 * numOfUsers+ "," + 16 * numOfUsers+ "," + 15 * numOfUsers+ "," + 14 * numOfUsers+ "," + 11 * numOfUsers+ "," + 16 * numOfUsers+ "," + 13 * numOfUsers+ "," + 13 * numOfUsers
+                    + "," + 15 * numOfUsers+ "," + 12 * numOfUsers+ "," + 13 * numOfUsers+ "," + 13 * numOfUsers+ "," + 16 * numOfUsers+ "," + 14 * numOfUsers+ "," + 16 * numOfUsers;
+            ;
+            return str;
+            // Missouri
+//        return "3985,6807,6557,6779,3909,3673,3641,3321,3148,4639,5350,5783,3585,3213,6655,4301,3262,5825,4743,4663,6338,6588,5335,3330,6897,4976,6971," +
+//                "6201,5570,4785,4972,6779,5466,3310,5407,4210,4170,5024,3092,6396,4556,4155,3972,4999,4346,3437,3509,6406,4675,6572,4162,4311,4362,4908,6254," +
+//                "5885,6857,5947,5472,6254,5455,3885,3437,3843,5813,5759,6817,";//uniform 3000-7000
+//        return "483,337,164,170,138,244,441,489,54,279,347,123,104,470,117,295,412,173,441,413,301,499,85," +
+//                "134,222,83,62,437,445,109,373,66,82,191,248,174,67,121,349,80,348,88,344,218,395,95,366,420," +
+//                "216,421,139,403,90,303,192,220,233,376,407,471,78,366,75,53,478,200,346,"; //uniform 50 500
+
+        } else if (graphModel.graphName.equals(Graph.NOEL)) {
+            // Noel
+            String str = "" + 50 * numOfUsers + "," + 50 * numOfUsers + "," + 48 * numOfUsers + "," + 40 * numOfUsers + "," + 43 * numOfUsers + "," + 59 * numOfUsers +
+                    "," + 69 * numOfUsers + "," + 66 * numOfUsers + "," + 50 * numOfUsers + "," + 60 * numOfUsers + "," + 66 * numOfUsers + "," + 54 * numOfUsers +
+                    "," + 68 * numOfUsers + "," + 42 * numOfUsers + "," + 60 * numOfUsers + "," + 55 * numOfUsers + "," + 47 * numOfUsers + "," + 66 * numOfUsers + "," + 48 * numOfUsers;
+            return str;
+            //return "483,337,164,170,138,244,441,489,54,279,347,123,104,470,117,295,412,173,441,"; //5281 uniform 50 500
+            //return "50,50,48,40,43,59,69,66,50,60,66,54,68,42,60,55,47,66,48,"; //1041 uniform 40 70
+        }
+        return null;
 
     }
+
     private double getNumberOfUserPerRegion(int node) {
         // Landa
         String[] users = numberOfUsersPerRegion.split(",");
@@ -284,9 +327,9 @@ public class ParameterHandler {
         return false;
     }
 
-    public double getDemandOfNode(int nodeIndex,int appIndex) {
+    public double getDemandOfNode(int nodeIndex, int appIndex) {
         double numberPerRegion = getNumberOfUserPerRegion(nodeIndex);
         double landaPerApp = getLandaPerApp(appIndex);
-        return numberPerRegion*landaPerApp;
+        return numberPerRegion * landaPerApp;
     }
 }
